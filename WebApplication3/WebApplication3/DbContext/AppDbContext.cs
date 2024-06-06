11f3aa45fcf59d20cff91d84a4384a8e7006ee27
s27540 <s27540@pjwstk.edu.pl>
2024-06-06 10:49:40 +0200

    Dodanie kontekstu

---
+++ WebApplication3/WebApplication3/DbContext/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication3.Models;
+
+namespace WebApplication3.DbContext;
+
+public class AppDbContext : Microsoft.EntityFrameworkCore.DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Doctor> Doctors { get; set; }
+    public DbSet<Patient> Patients { get; set; }
+    public DbSet<Medicament> Medicaments { get; set; }
+    public DbSet<Prescription> Prescriptions { get; set; }
+    public DbSet<Prescription_Medicament> Prescription_Medicaments { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Prescription_Medicament>()
+            .HasKey(pm => new { pm.IdMedicament, pm.IdPrescription });
+    }
+}
