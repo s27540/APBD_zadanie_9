ddf9c6a806f8167fcb95adc1a1b6520cad078a60
s27540 <s27540@pjwstk.edu.pl>
2024-06-06 10:47:25 +0200

    Dodanie modeli

---
+++ WebApplication3/WebApplication3/Models/Medicament.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication3.Models;
+
+public class Medicament
+{
+    public int IdMedicament { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Type { get; set; }
+    public ICollection<Prescription_Medicament> Prescription_Medicaments { get; set; }
+}
